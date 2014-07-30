dd316fab6a3917fa70e61dc2ca83cdd8d9b23573
brian <brian@thetigerparty.com>
2014-07-30 11:27:27 +0800

    project init

---
+++ vivianClothing/vivianClothing/Controllers/UploadController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace vivianClothing.Controllers
+{
+    public class UploadController : Controller
+    {
+        //
+        // GET: /Upload/
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Your upload page.";
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public void Upload()
+        {
+            foreach (string file in Request.Files)
+            {
+                var postedFile = Request.Files[file];
+                postedFile.SaveAs(Server.MapPath("~/UploadedFiles/") + postedFile.FileName);  //??Path.GetFileName(postedFile.FileName));
+
+            }
+        }
+
+    }
+
+}
