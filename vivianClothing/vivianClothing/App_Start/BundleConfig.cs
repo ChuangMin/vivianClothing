428c6379f2c46aadb3ad4588a0eeb44573b6de8a
brian <brian@thetigerparty.com>
2014-08-12 10:35:40 +0800

    implement bootstrape

--- vivianClothing/vivianClothing/App_Start/BundleConfig.cs
+++ vivianClothing/vivianClothing/App_Start/BundleConfig.cs
@@ -38,6 +38,10 @@ namespace vivianClothing
                         "~/Content/themes/base/jquery.ui.datepicker.css",
                         "~/Content/themes/base/jquery.ui.progressbar.css",
                         "~/Content/themes/base/jquery.ui.theme.css"));
+            bundles.Add(new StyleBundle("~/Scripts/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
+                     "~/Content/bootstrap.min.css"));
         }
     }
 }
